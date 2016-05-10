e2f87e8ee624eaa280dcb1c0354104d80944e22b
Alexis60 <alexis-lecompte@hotmail.fr>
2016-05-10 10:27:34 +0200

    Correction execice 4

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
- ?>
+ $mini = 1;
+ $maxi = 100;
+ for ($i = $mini; $i < $maxi; $i++) {
+   if ($i%3==0 && $i%5==0) {
+     echo "fIZZbUZZ";
+   }elseif ($i%5==0) {
+     echo "Buzz";
+   }elseif ($i%3==0) {
+     echo "Fizz";
+   }else {
+     echo "$i";}
+   }
+   ?>
